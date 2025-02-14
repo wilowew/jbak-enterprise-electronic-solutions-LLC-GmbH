6a43949426e3adf3bc81f3f377975ea4bab2c0b3
wilowew <peoplechtoto@gmail.com>
2025-02-15 02:45:29 +0300

    Первый коммит

---
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveX, moveY).normalized;
+        rb.linearVelocity = movement * moveSpeed;
+    }
+}
