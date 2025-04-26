33be3809a67b046a901ed9971801862ca51f6c38
Кирилл Чертков <kirill.chertkov.2005@mail.ru>
2025-04-26 22:39:19 +0300

    v.0.1.7 Работа над первой катсценой

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -1,11 +1,31 @@
 using UnityEngine;
+using System.Collections;
 
 public class DialogueTrigger : MonoBehaviour
 {
     [SerializeField] private Dialogue dialogue;
+    [SerializeField] private bool startAutomatically = false;
+
+    private bool hasTriggered = false;
+
+    private void Start()
+    {
+        if (startAutomatically)
+        {
+            StartCoroutine(StartDialogueAfterDelay());
+        }
+    }
+
+    private IEnumerator StartDialogueAfterDelay()
+    {
+        yield return new WaitForSeconds(3f);
+        TriggerDialogue();
+    }
 
     public void TriggerDialogue()
     {
+        if (hasTriggered) return;
+        hasTriggered = true;
         DialogueManager.Instance.StartDialogue(dialogue);
     }
 
